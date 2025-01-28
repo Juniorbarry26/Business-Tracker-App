d3f435c2360c0870c1b2e01b802682a50838c311
Alsainey Barry <alsainey7@gmail.com>
2025-01-28 18:14:32 +0000

    Create the express app to run it in the server

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,1 +1,16 @@
-console.log("Testing")
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON request bodies
+
+
+
+// routes
+
+// ports and mongodb URI
+const PORT = 3000;
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
