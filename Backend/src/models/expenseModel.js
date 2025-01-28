a2e02efd0bd3e1073c800925636ded8f776f0acc
Alsainey Barry <alsainey7@gmail.com>
2025-01-28 17:33:14 +0000

    Create the model for the expense

---
+++ Backend/src/models/expenseModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const expenseSchema = new mongoose.Schema({ 
+  amount: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ['groceries', 'transportation', 'entertainment', 'miscellaneous']
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 200
+  }
+})
