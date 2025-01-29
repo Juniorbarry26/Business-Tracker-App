50b3160286e8a473cf5d9cf606720e2dfce960d4
Alsainey Barry <alsainey7@gmail.com>
2025-01-29 13:19:16 +0000

    Update the route for the api endpoints

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const expenseRouter = require('./routes/expenseRoute');
+const budgetRouter = require('./routes/budgetRoute');
 
 // Middleware to parse JSON request bodies
 
-
-
 // routes
+app.use('/api/v1/expense', expenseRouter);
+app.use('/api/v1/budget', budgetRouter);
 
 // ports and mongodb URI
-const PORT = 3000;
+const PORT = 4000;
 
 // start the server
 app.listen(PORT, () => {
